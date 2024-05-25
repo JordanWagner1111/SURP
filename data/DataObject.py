35ddba5e8229efcdace8bae4340445fda976f113
Jordan Wagner <wagnerjordan1111@gmail.com>
2024-05-25 05:12:16 -0400

    Added WR data into plot

--- data/DataObject.py
+++ data/DataObject.py
@@ -1,4 +1,5 @@
 import os
+import warnings
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,8 +7,10 @@ import pandas as pd
 from astropy.coordinates import SkyCoord
 from astropy.io import fits
 from astropy.utils.data import get_pkg_data_filename
-from astropy.wcs import WCS
+from astropy.wcs import WCS, FITSFixedWarning
 from pkg_resources import resource_filename
+
+warnings.filterwarnings("ignore", category=FITSFixedWarning)
 
 
 class DataObject:
@@ -24,19 +27,28 @@ class DataObject:
 
         return hdu
 
+    # ===================================================================================
     def get_atomic_map_wcs(self):
         wcs = WCS(self.get_atomic_map_hdu().header)
 
         return wcs
 
-    def get_rsg_coord_table(self):
-        table = pd.read_csv("data/tsv/asu.tsv", sep="\t")
+    # ===================================================================================
+    def get_rsg_coord_df(self):
+        table = pd.read_csv("data/tsv/rsg_df.tsv", sep="\t")
 
         return table
 
+    # ===================================================================================
+    def get_wr_coord_df(self):
+        table = pd.read_csv("data/tsv/wr_df.tsv", sep="\t")
+
+        return table
+
+    # ===================================================================================
     def rsg_pixel_array(self):
-        RA = self.get_rsg_coord_table()["RAJ2000"]
-        Dec = self.get_rsg_coord_table()["DEJ2000"]
+        RA = self.get_rsg_coord_df()["RAJ2000"]
+        Dec = self.get_rsg_coord_df()["DEJ2000"]
 
         sky_hourangle = SkyCoord(RA, Dec, unit=("hourangle", "deg"))
         sky_deg = SkyCoord(sky_hourangle.ra.deg, sky_hourangle.dec.deg, unit=("deg"))
@@ -46,7 +58,21 @@ class DataObject:
 
         return i_component, j_component
 
-    def rsgs_atomic_map_array(self, hdu):
+    # ===================================================================================
+    def wr_pixel_array(self):
+        RA = self.get_wr_coord_df()["RA"]
+        Dec = self.get_wr_coord_df()["Dec"]
+
+        # Coordinates are already in degrees
+        sky_deg = SkyCoord(RA, Dec, unit=("deg"))
+        x, y = self.get_atomic_map_wcs().world_to_pixel(sky_deg)
+        i_component = np.asarray(x, dtype="int")
+        j_component = np.asarray(y, dtype="int")
+
+        return i_component, j_component
+
+    # ===================================================================================
+    def rsgs_on_gas_map_array(self, hdu):
         i_component, j_component = self.rsg_pixel_array()
 
         filterout = []
@@ -68,10 +94,36 @@ class DataObject:
 
         return i_component, j_component
 
-    def plot_rsg_cdf(
+    # ===================================================================================
+    def wrs_on_gas_map_array(self, hdu):
+        i_component, j_component = self.wr_pixel_array()
+        filterout = []
+        for idx in range(len(i_component)):
+            filterout.append(
+                i_component[idx] >= hdu.data.shape[0]
+                or j_component[idx] >= hdu.data.shape[1]
+            )
+        new_i_coordinate = []
+        new_j_coordinate = []
+        for idx in range(len(filterout)):
+            if not filterout[idx]:
+                new_i_coordinate.append(i_component[idx])
+                new_j_coordinate.append(j_component[idx])
+            else:
+                pass
+        i_component = np.array(new_i_coordinate)
+        j_component = np.array(new_j_coordinate)
+
+        return i_component, j_component
+
+    # ===================================================================================
+    def plot_stellar_cdf(
         self,
         hdu,
-        stars_on_map_array,
+        star_types: int,
+        stars_on_map_array: np.array,
+        stars_on_map_array2: np.array,
+        bins,
         title: str,
         xlabel: str,
         ylabel: str,
@@ -79,16 +131,26 @@ class DataObject:
     ):
         plt.hist(
             stars_on_map_array,
-            bins=np.logspace(0, 4, 1000),
+            bins=bins,
             zorder=2,
             histtype="step",
             cumulative=True,
             density=True,
-            label="RSGs",
+            label="WRs",
         )
+        if star_types > 1:
+            plt.hist(
+                stars_on_map_array2,
+                bins=bins,
+                zorder=2,
+                histtype="step",
+                cumulative=True,
+                density=True,
+                label="RSGs",
+            )
         plt.hist(
             hdu.data.flatten(),
-            bins=np.logspace(0, 4, 1000),
+            bins=bins,
             histtype="step",
             cumulative=True,
             density=True,
@@ -100,3 +162,4 @@ class DataObject:
         plt.title(title)
         plt.xlim(30, 2000)
         plt.legend(loc="upper left")
+        plt.grid()
