abc11f89c947f88450e2b4939e1df4eedf409907
Jordan Wagner <wagnerjordan1111@gmail.com>
2024-05-26 00:06:27 -0400

    Added subplots. Added new smoothed data and added LogLum data

--- data/DataObject.py
+++ data/DataObject.py
@@ -28,8 +28,27 @@ class DataObject:
         return hdu
 
     # ===================================================================================
+    def get_co_map_hdu(self):
+        """
+        Returns: co map Header Data Unit (hdu) and World Coordinate System (wcs)
+
+        """
+        filename = resource_filename(__name__, "FITS/ico.regrid.smooth.fits")
+        if not os.path.exists(filename):
+            raise FileNotFoundError(f"File {filename} not found.")
+        hdu = fits.open(filename)[0]
+
+        return hdu
+
+    # ===================================================================================
     def get_atomic_map_wcs(self):
         wcs = WCS(self.get_atomic_map_hdu().header)
+
+        return wcs
+
+    # ===================================================================================
+    def get_co_map_wcs(self):
+        wcs = WCS(self.get_co_map_hdu().header)
 
         return wcs
 
@@ -46,120 +65,78 @@ class DataObject:
         return table
 
     # ===================================================================================
-    def rsg_pixel_array(self):
-        RA = self.get_rsg_coord_df()["RAJ2000"]
-        Dec = self.get_rsg_coord_df()["DEJ2000"]
+    def get_star_pixel_array(self, star_type: str):
+        if star_type == "RSGs":
+            coord_df = self.get_rsg_coord_df()
+            RA = coord_df["RAJ2000"]
+            Dec = coord_df["DEJ2000"]
+            sky_coord_unit = ("hourangle", "deg")
+        elif star_type == "WRs":
+            coord_df = self.get_wr_coord_df()
+            RA = coord_df["RA"]
+            Dec = coord_df["Dec"]
+            sky_coord_unit = ("deg", "deg")
+        else:
+            raise ValueError(
+                "Expected 'RSGs' or 'WRs' for star_type, got '{}'".format(star_type)
+            )
 
-        sky_hourangle = SkyCoord(RA, Dec, unit=("hourangle", "deg"))
-        sky_deg = SkyCoord(sky_hourangle.ra.deg, sky_hourangle.dec.deg, unit=("deg"))
-        x, y = self.get_atomic_map_wcs().world_to_pixel(sky_deg)
+        sky_coord = SkyCoord(RA, Dec, unit=sky_coord_unit)
+        x, y = self.get_atomic_map_wcs().world_to_pixel(sky_coord)
         i_component = np.asarray(x, dtype="int")
         j_component = np.asarray(y, dtype="int")
 
         return i_component, j_component
 
     # ===================================================================================
-    def wr_pixel_array(self):
-        RA = self.get_wr_coord_df()["RA"]
-        Dec = self.get_wr_coord_df()["Dec"]
+    def stars_on_gas_map_array(self, hdu, star_type: str, map_type: str):
+        """
+        Filters and returns the pixel coordinates of stars on the gas map. It computes
+        the pixel coordinates of the specified star type using the World Coordinate System (WCS)
+        of the selected gas map. The function then filters out any coordinates that fall
+        outside the boundaries of the gas map.
 
-        # Coordinates are already in degrees
-        sky_deg = SkyCoord(RA, Dec, unit=("deg"))
-        x, y = self.get_atomic_map_wcs().world_to_pixel(sky_deg)
-        i_component = np.asarray(x, dtype="int")
-        j_component = np.asarray(y, dtype="int")
+        Args:
+            hdu (astropy.io.fits.PrimaryHDU): The primary HDU containing the gas map data.
+            star_type (str): The type of stars to be processed, either "RSGs" or "WRs".
+
+        Returns:
+            tuple: Two numpy arrays, `i_component` and `j_component`, containing the valid
+                pixel coordinates of the specified star type on the gas map.
+
+        Raises:
+            ValueError: If `star_type` is not "RSGs" or "WRs".
+        """
+        if star_type == "RSGs":
+            i_component, j_component = self.get_star_pixel_array(star_type="RSGs")
+        elif star_type == "WRs":
+            i_component, j_component = self.get_star_pixel_array(star_type="WRs")
+        else:
+            raise ValueError(
+                "Expected 'RSGs' or 'WRs' for star_type, got '{}'".format(star_type)
+            )
+
+        i_component = np.array(i_component)
+        j_component = np.array(j_component)
+
+        # Create a boolean mask for valid coordinates
+        if map_type == "Atomic":
+            valid_mask = (i_component < hdu.data.shape[0]) & (
+                j_component < hdu.data.shape[1]
+            )
+        elif map_type == "CO":
+            valid_mask = (i_component < hdu.data[0, 0].shape[0]) & (
+                j_component < hdu.data[0, 0].shape[1]
+            )
+        else:
+            raise ValueError(
+                "Expected 'Atomic' or 'CO' for map_type, got '{}'".format(map_type)
+            )
+
+        # Use the mask to filter the coordinates
+        i_component = i_component[valid_mask]
+        j_component = j_component[valid_mask]
 
         return i_component, j_component
 
     # ===================================================================================
-    def rsgs_on_gas_map_array(self, hdu):
-        i_component, j_component = self.rsg_pixel_array()
-
-        filterout = []
-        for idx in range(len(i_component)):
-            filterout.append(
-                i_component[idx] >= hdu.data.shape[0]
-                or j_component[idx] >= hdu.data.shape[1]
-            )
-        new_i_coordinate = []
-        new_j_coordinate = []
-        for idx in range(len(filterout)):
-            if not filterout[idx]:
-                new_i_coordinate.append(i_component[idx])
-                new_j_coordinate.append(j_component[idx])
-            else:
-                pass
-        i_component = np.array(new_i_coordinate)
-        j_component = np.array(new_j_coordinate)
-
-        return i_component, j_component
-
-    # ===================================================================================
-    def wrs_on_gas_map_array(self, hdu):
-        i_component, j_component = self.wr_pixel_array()
-        filterout = []
-        for idx in range(len(i_component)):
-            filterout.append(
-                i_component[idx] >= hdu.data.shape[0]
-                or j_component[idx] >= hdu.data.shape[1]
-            )
-        new_i_coordinate = []
-        new_j_coordinate = []
-        for idx in range(len(filterout)):
-            if not filterout[idx]:
-                new_i_coordinate.append(i_component[idx])
-                new_j_coordinate.append(j_component[idx])
-            else:
-                pass
-        i_component = np.array(new_i_coordinate)
-        j_component = np.array(new_j_coordinate)
-
-        return i_component, j_component
-
-    # ===================================================================================
-    def plot_stellar_cdf(
-        self,
-        hdu,
-        star_types: int,
-        stars_on_map_array: np.array,
-        stars_on_map_array2: np.array,
-        bins,
-        title: str,
-        xlabel: str,
-        ylabel: str,
-        gas_label: str,
-    ):
-        plt.hist(
-            stars_on_map_array,
-            bins=bins,
-            zorder=2,
-            histtype="step",
-            cumulative=True,
-            density=True,
-            label="WRs",
-        )
-        if star_types > 1:
-            plt.hist(
-                stars_on_map_array2,
-                bins=bins,
-                zorder=2,
-                histtype="step",
-                cumulative=True,
-                density=True,
-                label="RSGs",
-            )
-        plt.hist(
-            hdu.data.flatten(),
-            bins=bins,
-            histtype="step",
-            cumulative=True,
-            density=True,
-            label=gas_label,
-        )
-        plt.xscale("log")
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
-        plt.title(title)
-        plt.xlim(30, 2000)
-        plt.legend(loc="upper left")
-        plt.grid()
